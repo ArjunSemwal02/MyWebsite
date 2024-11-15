12c0b24899bb65c35eccb57f684d072190223ffb
Arjun <arjunsemwal02@gmail.com>
2024-11-15 14:39:02 +0530

    updated the contact section for email

--- source/script.js
+++ source/script.js
@@ -1,6 +1,12 @@
 const menuIconElement = document.querySelector('#menu-icon');
 const linksListElement = document.querySelector('.links');
+const emailIconElement = document.querySelector('.email-icon');
+const myEmailElement = document.querySelector('.my-email');
 
 menuIconElement.onclick = () => {
     linksListElement.classList.toggle('active');
+}
+
+emailIconElement.onclick = () => {
+    myEmailElement.classList.toggle('active');
 }
