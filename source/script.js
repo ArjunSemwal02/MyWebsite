6d89e2eec6d006d319b99e433739b7f943712410
Arjun <arjunsemwal02@gmail.com>
2024-11-20 12:46:46 +0530

    footer height updated

--- source/script.js
+++ source/script.js
@@ -2,6 +2,7 @@ const menuIconElement = document.querySelector('#menu-icon');
 const linksListElement = document.querySelector('.links');
 const emailIconElement = document.querySelector('.email-icon');
 const myEmailElement = document.querySelector('.my-email');
+const sectionElement = document.querySelectorAll('section');
 
 menuIconElement.onclick = () => {
     linksListElement.classList.toggle('active');
