3f808da62ce3db2fde16f673895d080b14ff46b2
Arjun <arjunsemwal02@gmail.com>
2025-08-18 14:06:58 +0530

    new data files are added for now
    for tests

--- src/app/pages/education/education.component.ts
+++ src/app/pages/education/education.component.ts
@@ -7,5 +7,18 @@ import { Component } from '@angular/core';
   styleUrl: './education.component.css'
 })
 export class EducationComponent {
-
+  educationList = [
+    {
+      programme: 'Bachelor of Technology',
+      specialization: 'Computer Science and Engineering',
+      institution: 'Govind Ballabh Pant Inst. of Eng. and Technology (UTU), Pauri garhwal, Uttarakhand',
+      duration: 'Grad. in 2023',
+    },
+    {
+      programme: 'Diploma',
+      specialization: 'Information Technology',
+      institution: 'Govt. Polytechnic Narendra Nagar (UBTER), Tehri Garhwal, Uttarakhand',
+      duration: 'Cmpl. Oct 2020',
+    }
+  ]
 }
